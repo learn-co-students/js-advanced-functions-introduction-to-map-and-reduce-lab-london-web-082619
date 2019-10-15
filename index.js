2ce06b16052e6c04695153f670cd1ab616cbc071
Angus Morrison <contact@angus-morrison.com>
2019-10-15 12:23:44 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+function mapToNegativize(array) {
+  return array.map(item => -item);
+}
+
+function mapToNoChange(array) {
+  return array.map(item => item);
+}
+
+function mapToDouble(array) {
+  return array.map(item => item * 2);
+}
+
+function mapToSquare(array) {
+  return array.map(item => item**2);
+}
+
+function reduceToTotal(array, initial = 0) {
+  return array.reduce(((total, item) => total + item), initial);
+}
+
+function reduceToAllTrue(array) {
+  return array.every(item => item);
+}
+
+function reduceToAnyTrue(array) {
+  return array.some(item => item);
+}
